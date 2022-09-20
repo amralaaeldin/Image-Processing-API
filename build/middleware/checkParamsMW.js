8e177ca7d252565f07435d8d18c2c57097685fed
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2022-09-20 10:41:37 +0200

    Delete build directory

--- build/middleware/checkParamsMW.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function checkParams(req, res, next) {
-    if (!req.query.filename)
-        return res.status(422).send('Unprocessable Entity: missing filename url query param');
-    next();
-}
-exports.default = checkParams;
