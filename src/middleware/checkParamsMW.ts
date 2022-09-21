68d4759bc5155c9aaec897b97f958c23a1c14f86
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-09-21 10:32:10 +0200

    fix: checking for width& height url query params

--- src/middleware/checkParamsMW.ts
+++ src/middleware/checkParamsMW.ts
@@ -5,10 +5,16 @@ export default function checkParams(
   req: express.Request,
   res: express.Response,
   next: Function
-) : void | express.Response {
+): void | express.Response {
   if (!req.query.filename)
     return res
       .status(422)
       .send('Unprocessable Entity: missing filename url query param');
+
+  if (req.query.width && !Number(req.query.width as string))
+    req.query.width = undefined
+
+  if (req.query.height && !Number(req.query.height as string))
+    req.query.height = undefined
   next();
 }
