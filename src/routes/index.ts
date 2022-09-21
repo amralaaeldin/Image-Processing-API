c5f2b1250e4cbe8c86461e4468d96ef4dbfb3cae
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-09-21 02:42:57 +0200

    fix: typing function

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import imgRoutes from './imgRoutes';
 const routes = express.Router();
 
-routes.get('/', (req: express.Request, res: express.Response) => {
+routes.get('/', (req: express.Request, res: express.Response) : void => {
   res.send('Welcome to Image Processing API!');
 });
 
